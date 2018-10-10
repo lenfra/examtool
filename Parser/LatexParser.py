c76062ec14219e1ec5688f2471628ed31a28178d
lenfra <lennart.franked@miun.se>
2018-10-10 14:21:37 +0200

    Fixed bug, if multiple citations on same line, the line was duplicated in the answer

--- Parser/LatexParser.py
+++ Parser/LatexParser.py
@@ -509,10 +509,11 @@ class LatexParser:
                     _citations = self.append_citation(line)
                     for _bib in _citations:
                         _question.append_bibliography_requirement(_bib)
-                        if _solution_exist:
-                            self._question_append_solution(_question, line)
-                        else:
-                            self._question_append_line(_question, line)
+                        
+                    if _solution_exist:
+                        self._question_append_solution(_question, line)
+                    else:
+                        self._question_append_line(_question, line)
 
             elif _start_solution.search(line):  # Find solution to _question
                 self.check_points(_question)
